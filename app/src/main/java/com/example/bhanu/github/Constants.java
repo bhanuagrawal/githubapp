0c01706580950274012bdc461ea7427ea9d007ca
bhanu agrawal <bhanuagrawal001@gmail.com>
2018-12-31 11:56:08 +0530

    !

---
+++ app/src/main/java/com/example/bhanu/github/Constants.java
@@ -0,0 +1,10 @@
+package com.example.bhanu.github;
+
+public class Constants {
+
+
+    public static final String GITHUB_CLIENT_SECRET  = "7365c2ac1b21c406714eef5a4d5b12e8beb8814e";
+    public static final String GITHUB_CLIENT_ID  = "4500bb3406d19e68164d";
+
+    public static final String REDIRECT_URL_CALLBACK = "bhanu://git.oauth2";
+}
